e7f37d9fc9a143a3cdb39425021c1246fa825b95
anbumani12 <anbumani15012002@gmail.com>
2024-04-03 06:48:19 +0530

    code

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -89,7 +89,7 @@ function Login() {
         <Grid
           container
           component="main"
-          sx={{ height: "100vh", width: "119%", marginLeft: "-137px" }}
+          sx={{ height: "100vh", width: "119%", marginLeft: "-132px" }}
         >
           <CssBaseline />
           <Grid
