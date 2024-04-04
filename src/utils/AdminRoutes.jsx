fb62684dd307cd7ce5f0589f308c0a38fb7c330b
anbumani12 <anbumani15012002@gmail.com>
2024-04-04 18:19:04 +0530

    Capstone Project

--- src/utils/AdminRoutes.jsx
+++ src/utils/AdminRoutes.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+
 function AdminRoutes({ children }) {
-  let role = sessionStorage.getItem("role");
-  let token = sessionStorage.getItem("token");
-  return token && (role === "superadmin" || role === "admin") ? (
-    children
-  ) : (
-    <Navigate to="/login" />
+  return (
+    <>
+      {children}
+    </>
   );
 }
 
